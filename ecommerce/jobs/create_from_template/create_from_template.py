42cb8022b29681fcf34897cd5d839a41134c5b6c
khalildaibes <khalildaibes1@gmail.com>
2024-10-13 22:00:07 +0300

    test12

--- ecommerce/jobs/create_from_template/create_from_template.py
+++ ecommerce/jobs/create_from_template/create_from_template.py
@@ -62,7 +62,7 @@ def setup_git_manager(project_directory, github_username):
 
 
 def deploy_sanity(sanity_project_dir, project_name,  args ,client_data_dict):
-    sanity_token = os.getenv("SANITY_AUTH_TOKEN")
+    sanity_token = os.getenv("SANITY_ADMIN_TOKEN")
     manager = SanityManager(sanity_project_dir, sanity_token)
     manager.check_sanity_version_conflict()
     #
