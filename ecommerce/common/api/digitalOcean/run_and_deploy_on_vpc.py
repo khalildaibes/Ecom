e32428160709e436f39b652c1e460ed43a6ccf8b
khalildaibes <khalildaibes1@gmail.com>
2024-10-24 05:56:08 +0300

    Updated Scriptvvvvv

--- ecommerce/common/api/digitalOcean/run_and_deploy_on_vpc.py
+++ ecommerce/common/api/digitalOcean/run_and_deploy_on_vpc.py
@@ -88,6 +88,7 @@ class VpcCommands:
             self.run_ssh_command("source ~/.bashrc")
             self.run_ssh_command("export NVM_DIR = '$HOME/.nvm'")
             self.run_ssh_command('''[-s "$NVM_DIR/nvm.sh"] & & \."$NVM_DIR/nvm.sh"''')
+            self.run_ssh_command('''[-s "$NVM_DIR/bash_completion"] & & \."$NVM_DIR/bash_completion"''')
             self.run_ssh_command("nvm install 20")
             self.run_ssh_command("nvm use 20")
             self.run_ssh_command("apt install npm")
