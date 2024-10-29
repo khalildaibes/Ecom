c391963ff81ef2ac3a90c006fe50d3cf2911f67c
khalildaibes <khalildaibes1@gmail.com>
2024-10-29 19:39:51 +0200

    setup0

--- ecommerce/common/api/digitalOcean/get_droplet_info.ps1
+++ ecommerce/common/api/digitalOcean/get_droplet_info.ps1
@@ -34,3 +34,4 @@ if ($LASTEXITCODE -ne 0) {
 # Execute the command using doctl
 C:\WINDOWS\system32\config\systemprofile\doctl\doctl.exe compute ssh $DropletID --ssh-command "sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config && sudo systemctl restart sshd && sudo ufw allow 22/tcp" --ssh-key-path "C:\Users\Admin\.ssh\id_ed25519"
 
+Write-Host "DROPLET_INFO: $DropletInfo"
